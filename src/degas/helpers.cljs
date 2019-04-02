84165e0e476ac02663334d1c895dd390b7dac21b
Rakhim Davletkaliyev <freetonik@gmail.com>
2019-04-02 22:06:01 +0300

    more hard work

--- src/degas/helpers.cljs
+++ src/degas/helpers.cljs
@@ -40,3 +40,12 @@
   "Do body with probability prob."
   `(if (<= (rand-int 100) (* ~prob 100))
        (do ~@body)))
+
+
+;; -------
+;; Sorting
+(defn sort-by-value-desc [x]
+  (into (sorted-map-by (fn [key1 key2]
+                     (compare [(get x key2) key2]
+                              [(get x key1) key1])))
+        x))
--- src/degas_client/core.cljs
+++ src/degas_client/core.cljs
@@ -2,8 +2,9 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [wscljs.client :as ws]
             [wscljs.format :as fmt]
-            [clojure.core.async :refer [>! <! go chan timeout alts!]]
-
+            [clojure.core.async :refer [>! <! go chan timeout]]
+            [cljs.reader :refer [read-string]]
+            [degas-client.tsm :refer [distances get-distance fitness-tsm]]
             [degas.individual :as i]
             [degas.crossover :as c]
             [degas.mutation :as m]
@@ -11,43 +12,39 @@
             [degas.selection :as s]
             [degas.evolution :as e]
             [degas.fitness :as f]
-            [degas.helpers :as h]
- ))
+            [degas.helpers :as h]))
 
-(def distances [[ 0   28  57  72  81  85  80  113 89  80  ]
-                [ 28  0   28  45  54  57  63  85  63  63  ]
-                [ 57  28  0   20  30  28  57  57  40  57  ]
-                [ 72  45  20  0   10  20  72  45  20  45  ]
-                [ 81  54  30  10  0   22  81  41  10  41  ]
-                [ 85  57  28  20  22  0   63  28  28  63  ]
-                [ 80  63  57  72  81  63  0   80  89  113 ]
-                [ 113 85  57  45  41  28  80  0   40  80  ]
-                [ 89  63  40  20  10  28  89  40  0   40  ]
-                [ 80  63  57  45  41  63  113 80  40  0   ]])
+(enable-console-print!)
 
-(defn get-distance [[a b]] (get-in distances [a b]))
+(declare comm-handlers)
+(declare send-message)
+(declare socket)
+(def server-addr "ws://localhost:3449")
 
-(defn fitness-tsm [ind]
-  "Returns the total distance travelled for a given solution"
-  (let [path (h/pathify ind)
-        distances (map get-distance path)]
-    (- (reduce + distances))))
+(def popsize   30)
+(def indlength 50)
 
-;; =====
 ;; State
+(defonce online? (atom false))
+(defonce authenticated? (atom false))
+(defonce ratings (atom {}))
+(defonce username (atom ""))
 (defonce generation (atom 0))
 (defonce population (atom []))
 (defonce current-best (atom (first @population)))
 (defonce best (atom (first @population)))
+(defonce mutation-rate (/ (rand-int 40) 100))
+(defonce elitism-rate (/ (rand-int 8) 100))
 
-(defonce mutation-rate (atom 0.2))
-(defonce elitism-rate (atom 0.01))
+(defonce selections-map (atom { s/tournament-selection 1, s/random-selection 1 }))
+(defonce mutations-map (atom { m/mut-swap 1, m/mut-shift 1}))
 
-;; ===========
 ;; Async state
 (defonce running? (atom false))
-(def queue (chan))
+(def queue (chan 10))
+(def upload-queue (chan 10))
 
+;; Evolution
 (defn update-best []
   (if (> (fitness-tsm @current-best) (fitness-tsm @best))
     (reset! best @current-best)))
@@ -57,101 +54,68 @@
           (sort-by fitness-tsm
                    (e/next-generation @population
                                       fitness-tsm
-                                      { s/tournament-selection 2,
-                                        s/random-selection 1}
+                                      @selections-map
                                       { c/crossover-ordered 1 }
-                                      { m/mut-swap 1, m/mut-shift 1}
+                                      @mutations-map
                                       @mutation-rate
-                                      @elitism-rate
-                                      ))))
+                                      @elitism-rate))))
 
-(defn stop-async []
-  (reset! running? false))
-
-(defn reset-state []
-  (stop-async)
-  (reset! population (into [] (sort-by fitness-tsm (p/rand-pop 10 10 i/rand-unique-ind))))
-  (reset! best (first @population))
-  (reset! current-best (first @population))
-  (reset! generation 0))
+;; Async evolution
+(defn stop-async [] (reset! running? false))
 
 (defn run-async []
   (reset! running? true)
+  ;; (reset! running? false)
 
-  (go
-    (while @running?
-      (>! queue 1)
-      (<! (timeout 150))))
+  (go (while @running?
+        (>! queue @generation)
+        (<! (timeout 100))))
+  ;; (go (>! queue @generation))
 
-  ;; CONSUMER
-  (go
-    (while true
-      (let [item (<! queue)]
+  (go (while true
+        (let [item (<! queue)]
+          (evolve-step)
+          (reset! current-best (first @population))
+          (update-best)
+          (swap! generation inc))))
+  )
 
-        (evolve-step)
-        (reset! current-best (first @population))
-        (update-best)
+;; (run-async)
+;; (stop-async)
 
-        (swap! generation inc)))))
+;; Upload
+(defn upload-best [ind sock]
+  (send-message {:best ind} sock))
 
+(defn run-upload-async [sock]
+  (go (while @running?
+        (>! upload-queue @generation)
+        (<! (timeout 1000))))
 
-;; (s/tournament-selection @population fitness-tsm)
+  (go (while true
+        (let [item (<! upload-queue)]
+          (upload-best @best sock)
+          ))))
 
-;; (map #(h/weighted-rand-choice { m/mut-swap 1, m/mut-shift 1}) (range 2))
-;; (m/mutate-pop (take 2 @population) { m/mut-swap 1, m/mut-shift 1} 1)
-;; (m/mutate-pop @population { m/mut-swap 1, m/mut-shift 1} 0.2)
+;; Communication
+(defn send-message [m sock] (ws/send sock m fmt/edn))
 
-;; (map (h/weighted-rand-choice { m/mut-swap 1, m/mut-shift 1}) (take 2 @population))
+(defn handle-message [m sock]
+  (cond
+    (:start m) (if-not @running? (do (run-async)
+                                     (run-upload-async sock)))
+    (:stop m)  (if @running?    (stop-async))
 
-;; (defn next-generation-2
-;;   []
-;;   (let [
-;;         popsize         (count @population)
-;;         elite-count     (Math/floor (* 0 popsize))
-;;         offspring-count (- popsize elite-count)
+    (:update m) (reset! ratings (:update m))
+    ))
 
-;;         crossover-fn    (h/weighted-rand-choice { c/crossover-ordered 1 })
-;;         mutation-fn     (h/weighted-rand-choice { m/mut-swap 1 })
+(def comm-handlers {:on-message #(handle-message (read-string (.-data %)) socket)
+                    :on-open    #(reset! online? true)
+                    :on-close   #(reset! online? false)})
+(def socket (ws/create server-addr comm-handlers))
 
-;;         sorted-pop      (sort-by fitness-tsm @population)
-;;         elite-pop       (into [] (take elite-count sorted-pop))
-;;         breedable-pop   (m/mutate-pop (take-last offspring-count sorted-pop)
-;;                                       mutation-fn
-;;                                       0.35)
-;;         ]
-
-;;     (into elite-pop (repeatedly offspring-count
-;;                                            #(apply crossover-fn
-;;                                                    (s/tournament-selection breedable-pop fitness-tsm)))
-
-;;                                )))
-
-
-
-
-
-
-
-
-
-;; ========
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(enable-console-print!)
-(defonce message (atom "Nothing yet..."))
-
+;; ---------------
+;; View components
 (defn render-run-button []
   (if @running?
     [:button.btn.btn-success.mr-3 {:on-click stop-async} "Pause"]
@@ -162,66 +126,139 @@
            :style {:width "100%"}
            :on-change callback}])
 
+(defn render-name-input []
+  [:div
+   [:input {:type "text"
+            :value @username
+            :on-change #(reset! username (-> % .-target .-value))}]
+   [:input {:type "button" :value "Enroll!"
+            :class "btn btn-success"
+            :on-click (fn []
+                        (reset! authenticated? true)
+                        (send-message {:name @username} socket))}]])
 
 (defn update-mutation-rate [e]
   (reset! mutation-rate (/ (int (.. e -target -value)) 100)))
-
 (defn render-mutation-rate-slider []
   (render-slider (* 100 @mutation-rate) 0 100 update-mutation-rate))
 
-(defn messages []
-  [:div [:h2 "Generation " @generation]
-   [:p
-    [:span (prn-str @mutation-rate)]
-    [:br]
-    [render-mutation-rate-slider]]
-   [render-run-button]
-   [:p
-    (prn-str @current-best)
-    "  --->  "
-    (prn-str (fitness-tsm @current-best))]
+(defn update-elitism-rate [e]
+  (reset! elitism-rate (/ (int (.. e -target -value)) 100)))
+(defn render-elitism-rate-slider []
+  (render-slider (* 100 @elitism-rate) 0 100 update-elitism-rate))
 
-   [:p (map #(prn-str %) (take 10 @population))]
+(defn render-ratings [r]
+  [:ol.pl-3.ml-1
+   (doall (map (fn [x]
+                 ^{:key (first x)}
+                 [:li {:class (if (= (first x) @username) "font-weight-bold" "")}
+                  (str (first x) " " (Math/abs (second x)))])
+               (filter #(not= (first %) nil) (h/sort-by-value-desc @ratings))))])
 
-   [:hr]
-   [:h2 "Best so far:"]
-   [:p
-    (prn-str @best)
-    "  --->  "
-    [:strong (prn-str (fitness-tsm @best))]]
+(defn render-control-panel []
+  [:div.card
+   [:div.card-header "God mode"]
+   [:div.card-body
+    [:div.row
 
-   [:div (prn-str @message)]])
+     [:div.col
+      [:span "Mutation rate " (Math/floor (* 100 @mutation-rate)) "%"]
+      [render-mutation-rate-slider]]
 
-(reagent/render-component [messages]
-                          (. js/document (getElementById "app")))
+     [:div.col
+      [:span "Elitism rate " (Math/floor (* 100 @elitism-rate)) "%"]
+      [render-elitism-rate-slider]]]
 
 
+     ]]
+  )
+
+(defn render-evol-status []
+  [:div.card
+   [:div.card-header "Status"]
+   [:div.card-body
+    [:div
+     [:h5 "Generation " @generation]
+
+     [:p
+      (prn-str @current-best)
+      "  --->  "
+      (prn-str (fitness-tsm @current-best))]
+
+     [:h5 "Best so far:"]
+     [:p
+      (prn-str @best)
+      "  --->  "
+      [:strong (prn-str (fitness-tsm @best))]]]]]
+  )
+
+(defn render-status-stripe [connected? computing?]
+  [:div.progress.rounded-0
+   {:style {:height "7px"}}
+   [:div.progress-bar
+    {:class (if connected?
+              (if computing? "bg-success progress-bar-animated progress-bar-striped"
+                             "bg-success")
+              "bg-warning")
+     :style {:width "100%", :height "7px"}}]])
+
+(defn render-dashboard []
+    [:div.row
+     [:div.col-9
+      [:h3.mb-4 "The World of " [:b @username]]
+      [render-control-panel]
+      [:div.mt-4]
+      [render-evol-status]
+      ]
+
+     [:div.col-3.border-left
+      [:h4.mb-3 "Global ratings"]
+      [render-ratings @ratings]
+      ]
+    ])
+
+(defn render-auth []
+  [:div.row.justify-content-center
+   [:div.col-4 [render-name-input]]])
+
+(defn render-offline-status []
+  [:div
+   [:h3.text-center.mt-4 "Offline :'("]])
+
+(defn app []
+  [:div
+   [render-status-stripe @online? @running?]
+
+   [:div.container-fluid.mt-3
+    (if @online?
+      (if @authenticated?
+        [render-dashboard]
+        [render-auth])
+      [render-offline-status])]])
+
+;; Reagent stuff
+(reagent/render-component [app] (. js/document (getElementById "app")))
 (defn on-js-reload [])
 
-;; (def handlers {:on-message (fn [e] (reset! message (cljs.reader/read-string (.-data e))))
-;;                :on-open    #(prn "Opening a new connection")
-;;                :on-close   #(prn "Closing a connection")})
+;; ||||||||||||||||||||
+;; Destroyers of worlds
+(defn randomize-population! []
+  (reset! population (into [] (sort-by fitness-tsm
+                                       (p/rand-pop popsize indlength i/rand-unique-ind)))))
 
-;; (def socket (ws/create "ws://localhost:3450" handlers))
+(defn reset-state! []
+  (stop-async)
+  (randomize-population!)
+  (reset! authenticated? false)
+  (reset! username "")
+  (reset! best (first @population))
+  (reset! current-best (first @population))
+  (reset! generation 0))
 
-;; (ws/send socket [[1] [2] [3]] fmt/edn)
-;; (ws/send socket {:name "Jasds"} fmt/edn)
-
-;; (ws/close socket)
+;; (reset-state!)
 
 
-;; (defn init! []
-;;   (let [ws (js/WebSocket. "ws://localhost:3450")]
-;;     (aset ws "onmessage" (fn [m] (swap! message (fn [] (aget m "data")))))))
-
-;; (defn evolve []
-;;   (dotimes [i 500]
-;;     (js/console.log "\n\n===Starting =====")
-;;     (evolve-step)
-;;     (reset! current-best (first @population))
-;;     (update-best)
-;;     (js/console.log "=================")
-;;     )
-;;   )
-
-;; (evolve)
+(randomize-population!)
+(reset! best (first @population))
+(reset! current-best (first @population))
+(reset! generation 0)
